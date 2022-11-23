9d941b25353193d77ff44a0d26bb4ac76761bfb1
Dmitry <dimonbyt@gmail.com>
2022-11-23 15:57:51 +0300

    Добавьте файлы проекта.

---
+++ TestCPlus/TestCPlus.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*int num = 10;
+	
+	cin >> num; // ReadLine
+	cout << "My number: " << num; // WriteLine
+	*/
+
+
+	/*
+	int n1, n2, mul;  // n1 * n2 = mul
+	cin >> n1;
+	cin >> n2;
+	mul = n1 * n2;
+	cout << n1 << " * " << n2 << " = " << mul << endl; // endl = \n
+	*/
+	
+
+
+	return 0;
+}
